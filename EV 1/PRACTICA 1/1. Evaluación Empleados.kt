b08321dc6b9e01faf84c5721345be0ef44964538
AngelaM <104018585+Mila21xy@users.noreply.github.com>
2025-03-25 10:14:14 -0500

    Modificando 1. Evaluación Empleados.kt
    Agregando la descripción de los ejercicios

--- EV 1/PRACTICA 1/1. Evaluación Empleados.kt
+++ EV 1/PRACTICA 1/1. Evaluación Empleados.kt
@@ -5,6 +5,34 @@ package `PRACTICA 1`
     Curso: Programacion De Dispositivos Moviles
 */
 
+/*
+Ejercicio 1: Evaluación Empleados
+En una determinada empresa, sus empleados son evaluados cada 
+seis meses. Los puntos que pueden obtener en la evaluación 
+comienzan en 0 y pueden ir aumentando hasta llegar a 10, 
+traduciéndose en mejores beneficios. Al final del problema se 
+muestra una tabla con los niveles correspondientes a cada
+puntuación. La cantidad de dinero conseguida en cada nivel se 
+calcula multiplicando el salario mensual por la  división de 
+la puntuación del nivel divida entre 10. Escribir un programa 
+que lea la puntuación del usuario y su salario mensual e 
+imprima su nivel de rendimiento, así como la cantidad de dinero
+que recibirá el usuario. 
+Ejemplo: Salario 10,000; Puntuación 8.
+Dinero = 10,000 * (8/10)= 8000. Resultado: Nivel de Rendimiento Aceptable, Cantidad de Dinero Recibido $8000. 
+
+     +---------------+--------------------+
+     |     Nivel     |     Puntuación     |
+     +---------------+--------------------+
+     |  Inaceptable  |       0 a 3        |
+     +---------------+--------------------+
+     |   Aceptable   |       4 a 6        |
+     +---------------+--------------------+
+     |   Meritorio   |       7 a 10       |
+     +---------------+--------------------+
+
+*/
+
 // Funcion que evalua que la puntuacion este dentro del rango
 fun evaluarRango(puntuacion: Int): Boolean {
     return puntuacion in 0..10
@@ -36,8 +64,3 @@ fun main() {
     // Evaluar a un empleado
     println(evaluarEmpleados(10000.0,8))
 }
-
-/*
-    Ejemplo: Salario 10,000; Puntuación 8. Dinero = 10,000 * (8/10)= 8000.
-    Resultado: Nivel de Rendimiento Meritorio, Cantidad de Dinero Recibido $8000
-*/
