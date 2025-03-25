85eeeca8ff56172ced510cbacba309669b88ed86
AngelaM <104018585+Mila21xy@users.noreply.github.com>
2025-03-25 10:08:38 -0500

    Modificando 4. Sistema de Gestión de Biblioteca.kt
    Agregando la descripción de los ejercicios

--- EV 1/PRACTICA 2/4. Sistema de Gestión de Biblioteca.kt
+++ EV 1/PRACTICA 2/4. Sistema de Gestión de Biblioteca.kt
@@ -5,6 +5,30 @@ package `PRACTICA 2`
     Curso: Programación De Dispositivos Móviles  
 */
 
+/*
+Ejercicio 4: Sistema de Gestión de Biblioteca
+Diseña un sistema de gestión de biblioteca que incluya las siguientes 
+clases e interfaces:
+Material: Una clase base abstracta que tiene propiedades como título, 
+autor, anioPublicacion,  y un método abstracto para mostrarDetalles().
+Libro: Una subclase de Material que tiene propiedades adicionales como 
+género, número páginas y un método para mostrar los detalles del libro.
+Revista: Una subclase de Material que tiene propiedades como issn,
+volumen, número, editorial y un método para mostrar los detalles de la
+revista.
+Usuario: Una data class que tiene propiedades como nombre, apellido, edad.
+IBiblioteca:Una interfaz que contenga los métodos registrar material,
+registrar usuario, préstamo, devolución, mostrar materiales disponibles y
+mostrar materiales reservados por usuario.
+Biblioteca: Una clase que gestiona las operaciones de préstamo y devolución.
+Debe implementar la interfaz anterior. Debe tener una lista de materiales 
+disponibles, así como un Map de usuarios donde la clase es el usuario y el
+valor una lista de materiales que tiene en préstamo.
+Crear instancias de las subclases y ejecutar las operaciones de préstamo y
+devolución para cada instancia. 
+OJO: En este caso se cambio 'anioPublicación' por 'fechaPublicacion'
+*/
+
 // Clase base abstracta para materiales de la biblioteca
 abstract class Material(
     val titulo: String,
